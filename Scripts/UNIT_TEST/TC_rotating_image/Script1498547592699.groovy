14bba99626940d5feae23e93ca552dca047c6a72
hanjeho <hanjeho@mulan101>
2017-06-27 19:04:40 +0900

    unit_test rotating image 수정

--- Scripts/UNIT_TEST/TC_rotating_image/Script1498547592699.groovy
+++ Scripts/UNIT_TEST/TC_rotating_image/Script1498547592699.groovy
@@ -21,19 +21,32 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser('')
 
-WebUI.setViewPortSize(400, 700)
+not_run: WebUI.setViewPortSize(400, 700)
 
 WebUI.navigateToUrl('http://www.samsung.com/sec/smartphones/')
 
+'\uD68C\uC804 \uD655\uC778\uC744 \uC704\uD574 \uC2A4\uD06C\uB864 \uD3EC\uCEE4\uC2A4\uB97C \uB300\uC0C1 \uC5D8\uB9AC\uBA3C\uD2B8\uB85C \uC774\uB3D9\uD568'
 WebUI.scrollToElement(findTestObject('UNIT_TEST/RE_ROTATING_IMAGE/04.div_primary-selection__product'), 2)
 
-iLeft = Mobile.getElementLeftPosition(findTestObject('UNIT_TEST/RE_ROTATING_IMAGE/04.div_primary-selection__product'), 2)
+WebUI.delay(3)
 
-iTop = Mobile.getElementTopPosition(findTestObject('UNIT_TEST/RE_ROTATING_IMAGE/04.div_primary-selection__product'), 2)
+'\uCCAB\uBC88\uC9F8 \uC774\uB3D9'
+WebUI.dragAndDropToObject(findTestObject('UNIT_TEST/RE_ROTATING_IMAGE/01.img_js-img-src primary-selecti'), findTestObject(
+        'UNIT_TEST/RE_ROTATING_IMAGE/04.div_primary-selection__product'))
 
-WebUI.dragAndDropByOffset(findTestObject('UNIT_TEST/RE_ROTATING_IMAGE/01.img_js-img-src primary-selecti'), iLeft, iTop)
+WebUI.delay(3)
 
-WebUI.delay(5)
+'\uB450\uBC88\uC9F8 \uC774\uB3D9'
+WebUI.dragAndDropToObject(findTestObject('UNIT_TEST/RE_ROTATING_IMAGE/02.img_js-img-src primary-selecti'), findTestObject(
+        'UNIT_TEST/RE_ROTATING_IMAGE/05.div_primary-selection__product'))
+
+WebUI.delay(3)
+
+'3\uBC88\uC9F8 \uC774\uB3D9'
+WebUI.dragAndDropToObject(findTestObject('UNIT_TEST/RE_ROTATING_IMAGE/03.img_js-img-src primary-selecti'), findTestObject(
+        'UNIT_TEST/RE_ROTATING_IMAGE/06.div_primary-selection__product'))
+
+WebUI.delay(3)
 
 WebUI.closeBrowser()
 
